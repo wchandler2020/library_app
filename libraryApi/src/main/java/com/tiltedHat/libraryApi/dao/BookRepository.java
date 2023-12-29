d316a2dc6701b890c336c97b708e0fcd86b00d02
wchandler2020 <william.d.chandler1@gmail.com>
2023-12-28 19:33:43 -0600

    added functionality for search and pagination

--- libraryApi/src/main/java/com/tiltedHat/libraryApi/dao/BookRepository.java
+++ libraryApi/src/main/java/com/tiltedHat/libraryApi/dao/BookRepository.java
@@ -1,7 +1,13 @@
 package com.tiltedHat.libraryApi.dao;
 
 import com.tiltedHat.libraryApi.entity.Book;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
 
 public interface BookRepository extends JpaRepository<Book, Long> {
+    Page<Book> findByTitleContaining(@RequestParam("title") String title, Pageable pageable);
 }
